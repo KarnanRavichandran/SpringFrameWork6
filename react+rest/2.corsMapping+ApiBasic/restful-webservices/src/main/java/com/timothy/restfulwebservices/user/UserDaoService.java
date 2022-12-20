8cb4b3bb657320dda7f787a6ba50f183b9b6bec5
paultimothy12 <37688619+paultimothy12@users.noreply.github.com>
2022-12-20 19:57:41 +0530

    Add files via upload

---
+++ react+rest/2.corsMapping+ApiBasic/restful-webservices/src/main/java/com/timothy/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,50 @@
+package com.timothy.restfulwebservices.user;
+
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+    private static Integer userCount=0;
+    static{
+        users.add(new User(++userCount,"paul", LocalDate.now().minusYears(23)));
+        users.add(new User(++userCount,"nive", LocalDate.now().minusYears(23)));
+        users.add(new User(++userCount,"akshi", LocalDate.now().minusYears(13)));
+    }
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User findOne(Integer id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+//        return users.stream().filter(predicate).findFirst().get(); //return the user or else it throws null pointer exception
+        return users.stream().filter(predicate).findFirst().orElse(null);
+        //if there's no user, it doesn't throw whitelabel error but blank page, instead we need something else,
+        // go to the method in the controller and make changes
+
+    }
+
+
+    public User save(User user){
+        user.setId(++userCount);
+        users.add(user);
+        return user;
+    }
+
+
+    public void deleteById(Integer id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+//        return users.stream().filter(predicate).findFirst().get(); //return the user or else it throws null pointer exception
+//        return users.stream().filter(predicate).findFirst().orElse(null);
+        //if there's no user, it doesn't throw whitelabel error but blank page, instead we need something else,
+        // go to the method in the controller and make changes
+
+    }
+}
