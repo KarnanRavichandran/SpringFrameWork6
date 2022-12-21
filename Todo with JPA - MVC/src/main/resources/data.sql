f48ea831b60bdd1a180da35bff7935520ade1524
paultimothy12 <37688619+paultimothy12@users.noreply.github.com>
2022-12-21 08:18:34 +0530

    Add files via upload

---
+++ Todo with JPA - MVC/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'paul', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'paul', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'paul', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'paul', 'Learn DevOps', CURRENT_DATE(), false);
